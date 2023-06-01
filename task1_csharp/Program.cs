3e3371abe04dabee956f145250402960ab3716d7
Huseynov-Samir <samir.huseynov.2395@gmail.com>
2023-06-02 00:53:22 +0400

    level 5

--- task1_csharp/Program.cs
+++ task1_csharp/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+we add variable and understand what is it null
+*/
+
+string name;
+name=Console.ReadLine();
+Console.WriteLine(name);
