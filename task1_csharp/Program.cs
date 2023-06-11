156da3b11589358b65b15395e7feb16b416c829f
Huseynov-Samir <samir.huseynov.2395@gmail.com>
2023-06-11 19:55:51 +0400

    level9

--- task1_csharp/Program.cs
+++ task1_csharp/Program.cs
@@ -1,7 +1,24 @@
-﻿/*
-we add variable and understand what is it null
-*/
+﻿Console.Write("add your text ");
+string? name = Console.ReadLine();
+Console.Write("add your number ");
+int? age  = Convert.ToInt16(Console.ReadLine());
+char addSymbol = '?';
 
-string name;
-name=Console.ReadLine();
-Console.WriteLine(name);
+if(name=="g" || age==1){
+	name = Console.ReadLine();
+	age  = Convert.ToInt16(Console.ReadLine());
+	if(name=="admin" && age==12){
+		addSymbol='R';
+	}
+	else{
+		addSymbol='A';
+		string text= age>30?"yes it is possible":"no it isnt possible";
+		Console.WriteLine(text);
+	}
+}else if(name=="guset" && age==10){
+	addSymbol='B';
+}else{
+	addSymbol='C';
+}
+
+Console.WriteLine(name+" "+age+" "+addSymbol);
